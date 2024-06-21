28150587ad49c993541c45772f6c924eef14e6cc
nsnans <3064254883@qq.com>
2024-06-21 07:55:22 +0000

    Deploying to page from @ nsnans/copy_ui@58d0c9c8d51a6c43fefb2b98034696c572ce502e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "128740652"
+    serviceWorkerVersion: "2027535781"
   }
 });
